8a47fc536062fb5a12a139f39666ecd7166c6cbb
Jorge Zetien <jzettien@hotmail.com>
2025-03-31 22:13:11 -0500

    Enviroment variables key configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:cinemapedia/config/router/app_router.dart';
 import 'package:cinemapedia/config/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future main() async {
+  await dotenv.load(fileName: '.env');
+
   runApp(const MainApp());
 }
 
@@ -11,11 +14,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp.router(
+    return MaterialApp.router(
       routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
       theme: AppTheme().getTheme(),
-      
     );
   }
 }
