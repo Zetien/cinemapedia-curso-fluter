1c56372273c09a8b8b19106bf3668ed846cafa21
Jorge Zetien <jzettien@hotmail.com>
2025-04-06 14:41:07 -0500

    creado scroll infinito horizontal para peliculas

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,18 +1,54 @@
-import 'package:cinemapedia/config/constants/enviroment.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:cinemapedia/presentation/widgets/shared/widgets.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
-  static const name = 'home-screen';
+  static const name = 'hoimme-screen';
 
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(Enviroment.theMovieDbKey),
-      ),
+    return const Scaffold(
+      body: _HomeView(),
+      bottomNavigationBar: CustonBottomNavigation(),
+      //bottomNavigationBar: Placeholder(),
+    );
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+  @override
+  void initState() {
+    super.initState();
+
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    final slidesShowMovies = ref.watch(moviesSlideshowProvider);
+
+    return Column(
+      children: [
+        const CustomAppbar(),
+        MoviesSlideshow(movies: slidesShowMovies),
+        MovieHorizontalListview(
+          movies: nowPlayingMovies,
+          title: 'En Cines',
+          loadNextPage: ()=> ref.read(nowPlayingMoviesProvider.notifier).loadNextPage()
+          
+        )
+      ],
     );
   }
 }
